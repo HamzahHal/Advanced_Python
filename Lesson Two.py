84253cf0fd4004f535cfdaf732d0dc0642a465fd
HamzahHal <hamzah.halim@hotmail.com>
2021-08-11 15:10:56 +0100

    commit

---
+++ Lesson Two.py
@@ -0,0 +1,34 @@
+# Data Types
+int_type = 1
+float_type = 1.2
+bool_type = True
+str_type = "Test"
+list_type = list('dsa')
+tuple_type = tuple("adsasda")
+set_type = set('dsad')
+dict_type = {13123: 323232,
+             'no': 3203}
+
+print(int_type)
+print(float_type)
+print(str_type)
+print(list_type)
+print(tuple_type)
+print(set_type)
+print(dict_type)
+
+print(type(1))
+print(type(2 - 4))
+print(type(2 * 4))
+print(type(2 / 4))  # 0.5
+print(type(0.5343))
+
+print(2 ** 3)
+print(float(2 // 4))
+print(9 % 5)
+
+# Classes -> custom types
+
+# Specialized Data Types
+# Modules
+None
