41ae447abab61af02a04bb84026f590ebb897755
HamzahHal <hamzah.halim@hotmail.com>
2021-08-12 18:04:05 +0100

    Commit

--- Password Checker.py
+++ Password Checker.py
@@ -1,7 +1,14 @@
-Username = input("Enter username: ")
-Password = input("Enter Password: ")
+# Username = input("Enter username: ")
+# Password = input("Enter Password: ")
 
-hidden_password = '*' * len(Password)
+# print(f'password is {len(Password)} and username is {len(Username)} letters long')
+# print(f'Your password is {hidden_password} and your username is "{Username}"')
 
-print(f'password is {len(Password)} and username is {len(Username)} letters long')
-print(f'Your password is {hidden_password} and your username is "{Username}"')
+
+Card_Number = '543579349573495'
+
+hidden_number = '*' * (len(Card_Number) - len(Card_Number[10:])) + Card_Number[10:]
+
+print(len(Card_Number))
+print(len(hidden_number))
+print(hidden_number)
