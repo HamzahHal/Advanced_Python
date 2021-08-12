23eeb7d2dbf736b76273caadb8c10fdfd7839d11
HamzahHal <hamzah.halim@hotmail.com>
2021-08-12 17:54:27 +0100

    Commit

--- Password Checker.py
+++ Password Checker.py
@@ -3,5 +3,5 @@ Password = input("Enter Password: ")
 
 hidden_password = '*' * len(Password)
 
-print(f'password is {len(Password)} and usename is {len(Username)} letters long')
+print(f'password is {len(Password)} and username is {len(Username)} letters long')
 print(f'Your password is {hidden_password} and your username is "{Username}"')
