f673f5f50c8e8cdcabe90a264ccc86558fd6f32a
HamzahHal <hamzah.halim@hotmail.com>
2021-08-14 15:07:32 +0100

    Commit

--- Lesson Two.py
+++ Lesson Two.py
@@ -35,18 +35,62 @@ print(round(3.2))
 print(abs(-342))
 
 # list - type of array (data type)
-li = [1, 2, 3, 4, 5]
-li2 = ['c', 'v', 'b']
-li3 = [1.5, 3, 4.54, 's', False]
-amazon_cart = ['notepad', 'testcare']
-print(amazon_cart[0])
-new_value = 'test'
-new_value2 = 'fello'
-amazon_cart.insert(1, 'testewr')
-amazon_cart.append(new_value)
-amazon_cart.append([new_value, new_value2])
-print(amazon_cart)
+# li = [1, 2, 3, 4, 5]
+# li2 = ['c', 'v', 'b']
+# li3 = [1.5, 3, 4.54, 's', False]
+# amazon_cart = ['notepad', 'testcare']
+# print(amazon_cart[0])
+# new_value = 'test'
+# new_value2 = 'fello'
+# amazon_cart.insert(1, 'testewr')
+# amazon_cart.append(new_value)
+# amazon_cart.append([new_value, new_value2])
+# print(amazon_cart)
+
+new_cart = [
+    'notework',
+    'sunglasses',
+    'toys',
+    'grapes',
+    'shoes'
+]
+new_cart[0] = 'Laptop'
+# duplicate instead of just changing
+newer_cart = new_cart
+newer_cart = new_cart[:]
+newer_cart[0] = 'Gumo'
+print(newer_cart)
+print(new_cart)
+
+# Matrix
+matrix = [
+    [1,2,3],
+    [3,4,5],
+    [7,8,9]
+]
+
+print(matrix[0][1])
+for num in matrix:
+    print(num)
+
+# Adding
+basket = [1,2,3,4,5]
+new_item = basket[:]
+new_item.insert(0,100)
+new_item.extend([1000, 10001])
+print(basket)
+print(new_item)
+
+# removing
+basket.remove(1)
+print(basket)
+basket.pop()
+print(basket)
+basket.pop(2)
+print(basket)
+
 # Classes -> custom types
+
 
 # Specialized Data Types
 # Modules
