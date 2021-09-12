02bb5bd8f40d773fc930c1a852597419d7e2d996
HamzahHal <hamzah.halim@hotmail.com>
2021-09-12 14:33:25 +0100

    Commit

---
+++ Loops.py
@@ -0,0 +1,49 @@
+# for item in 'Zero to Mastery':
+#     print(item)
+#
+# for item in range(10):
+#     print(item)
+#
+# for item in (1,2,3,4,5):
+#     print(item)
+#
+# for item in {1,2,3,4,5}:
+#     print(item)
+#
+# for item in [1,2,3,4,5]:
+#     print(item)
+#     item += 1
+#     print(item)
+
+# user = {
+#     'name' :  'Golem',
+#     'age': 2200,
+#     'can_swim': False
+#
+# }
+#
+# for key, value in user.items():
+#    print(key, value)
+#
+# for item in user.values():
+#     print(item)
+#
+# for item in user.keys():
+#     print(item)
+
+
+# for item in (1,2,3,4,5):
+#     for x in ['a','b','c']:
+#         print(item, x)
+# Iterables -> list, dictionary, tuple, set , string, range
+# Iterated -> one by one to check each item in the collection
+print("hellow world")
+
+# Counter
+my_list = [1,2,3,4,5,6,7,8,9,10]
+counter = 0
+
+for items in my_list:
+    counter += items
+
+print(counter)
