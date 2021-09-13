bc324f58fe2a0b96533e0cc895b9377b650c84f7
HamzahHal <hamzah.halim@hotmail.com>
2021-09-13 15:19:59 +0100

    Commit

--- First GUI.py
+++ First GUI.py
@@ -8,12 +8,33 @@ picture = [
     [0, 0, 0, 1, 0, 0, 0],
     [0, 0, 0, 1, 0, 0, 0]
 ]
-
+# Attempted Exercise
 i = ' '
 k = '*'
+image = []
 
-for num in picture:
-    if num == 0:
-        print(i)
-    else:
-        print(k)
+for index, integer in enumerate(picture):
+    if integer == 0:
+        space = ' '
+        image[index] += space
+    elif integer == 1:
+        star = '*'
+        image[index] += star
+    print(image)
+
+# for num in picture:
+#     if num == 0:
+#         image.append(i)
+#     elif num == 1:
+#         image.append(k)
+#         continue
+print(image)
+
+# Solution
+for row in picture:
+    for pixel in row:
+        if pixel == 1:
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print('')
