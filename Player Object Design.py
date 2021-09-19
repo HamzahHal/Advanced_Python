b134caeb2f53b438d42de844a68ca6812b368e1e
HamzahHal <hamzah.halim@hotmail.com>
2021-09-19 03:38:47 +0100

    Commit

--- Player Object Design.py
+++ Player Object Design.py
@@ -20,18 +20,22 @@ player2.expBar = 0
 player2.expMax = 100
 player2.expCount = 0
 player2.hit = False
+x = int(input("Enter Value: "))
+if x > 1:
+    player2.hit = True
 
 if player2.hit:
     player2.expCount += 1
 
 if player2.expCount > 0:
-    player2.expBar += 1
+    player2.expBar += 100
 
 if player2.expBar == player2.expMax:
     player2.health += (60 / 2)
     player2.attack += (100 / 2)
-print(run())
-print(player2.attack)
-print(player2.speed)
-print(player2.health)
-print(player2.power)
+
+# print(run())
+print(f'attack {player2.attack}')
+# print(f'speed {player2.speed}')
+print(f'health {player2.health}')
+# print(f'power {player2.power}')
