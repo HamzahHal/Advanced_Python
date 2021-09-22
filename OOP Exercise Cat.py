401799f8503fbe5556744a84a2d498972905eec1
HamzahHal <hamzah.halim@hotmail.com>
2021-09-22 03:02:03 +0100

    Commit

---
+++ OOP Exercise Cat.py
@@ -0,0 +1,26 @@
+# Given the below class:
+class Cat:
+    species = 'mammal'
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+# 1 Instantiate the Cat object with 3 cats
+Cat1 = Cat('Tom', 30)
+Cat2 = Cat('Jerry', 32)
+Cat3 = Cat('Callum', 2230)
+
+
+# 2 Create a function that finds the oldest cat
+def findOldest():
+    oldest = 0
+    my_list = [Cat1.age, Cat2.age, Cat3.age]
+    for i in my_list:
+        if i > oldest:
+            oldest = i
+    return oldest
+
+# 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
+print(f'The oldest cat is the {findOldest()}.')
--- Player Object Design.py
+++ Player Object Design.py
@@ -1,17 +1,24 @@
 # OOP
-def run():
-    print('run')
-    return 'Done'
 
 
 class PlayerCharacter:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    # Class object attribute (static , not dynamic)
+    membership = True
+
+    def __init__(self, name='Anonymous', age=0):
+        if age > 18:
+            self.name = name  # Attributes
+            self.age = age
+
+    def shout(self):
+        print(f'my name is {self.name}')
+
+    def run(self, hello):
+        print(f'name name name {self.name}')
 
 
-player1 = PlayerCharacter('Hamzah', 22)
-player2 = PlayerCharacter('Tom', 55)
+#player1 = PlayerCharacter('collu', 50)
+player2 = PlayerCharacter('Tom', 43)
 player2.attack = 50
 player2.speed = 34
 player2.health = 10000
@@ -20,22 +27,26 @@ player2.expBar = 0
 player2.expMax = 100
 player2.expCount = 0
 player2.hit = False
-x = int(input("Enter Value: "))
-if x > 1:
-    player2.hit = True
+# x = int(input("Enter Value: "))
+# if x > 1:
+#     player2.hit = True
+#
+# if player2.hit:
+#     player2.expCount += 1
+#
+# if player2.expCount > 0:
+#     player2.expBar += 100
+#
+# if player2.expBar == player2.expMax:
+#     player2.health += (60 / 2)
+#     player2.attack += (100 / 2)
 
-if player2.hit:
-    player2.expCount += 1
-
-if player2.expCount > 0:
-    player2.expBar += 100
-
-if player2.expBar == player2.expMax:
-    player2.health += (60 / 2)
-    player2.attack += (100 / 2)
-
-# print(run())
-print(f'attack {player2.attack}')
-# print(f'speed {player2.speed}')
-print(f'health {player2.health}')
-# print(f'power {player2.power}')
+#print(player1.run('hiiiiiiiiiiiiiii'))
+# print(f'attack {player2.attack}')
+# # print(f'speed {player2.speed}')
+# print(f'health {player2.health}')
+# # print(f'power {player2.power}')
+#print(player2.membership)
+#print(player1.shout())
+print(player2.attack)
+print(player2.name)
