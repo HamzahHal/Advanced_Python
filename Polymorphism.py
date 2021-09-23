7dc214f8cf1da8e979adeac6feef439a16b26ea2
HamzahHal <hamzah.halim@hotmail.com>
2021-09-23 20:29:27 +0100

    Commit

--- Polymorphism.py
+++ Polymorphism.py
@@ -8,13 +8,20 @@ class User:
         if accessUser and accessPass:
             print(accessUser)
 
+    # accessing this method through the Wizard attack method
+    def attack(self):
+        print('do nothing')
 
+
+# Here I inherit the User class and this becomes a sub class
 class Wizard(User):
     def __init__(self, name, element):
         self.name = name
         self.element = element
 
     def attack(self):
+        # Here is where I'm accessing the main parent classes method
+        User.attack(self)
         print(f'Attacking with {self.element}')
 
 
@@ -25,13 +32,13 @@ class Archer(User):
 
     def attack(self):
         print(f'{self.name} is beginning their attack, be prepared!')
-        arrows_left = 0
         attack_num = int(input('Enter amount of arrows you want to shoot: '))
         if attack_num <= self.arrows:
             arrow_left = self.arrows - attack_num
             print(f'Attacking with {attack_num} arrows, you now have {arrow_left} arrows left ')
         else:
             print(f"I'm sorry {self.name}, you don't have enough arrows to make this attack. Currently you only hold {self.arrows}")
+        self.arrows = arrow_left
 
 
 # Instantiated objects
@@ -39,6 +46,16 @@ wizard1 = Wizard('Dr. Strange', 'Fire')
 
 archer1 = Archer('Green Arrow', 100)
 
-wizard1.attack()
+# def player_attack(char):
+#     char.attack()
+
+# for char in [wizard1, archer1]:
+#     char.attack()
+print(wizard1.attack())
+# player_attack(wizard1)
+# player_attack(wizard1)
+# player_attack(archer1)
+# wizard1.attack()
 # archer1.attack()
-print(isinstance(wizard1, object))
+# print(archer1.arrows)
+# print(isinstance(wizard1, object))
