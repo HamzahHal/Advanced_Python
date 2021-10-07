59b1f166e21260d6f2df278dafe169b9f29587b9
HamzahHal <hamzah.halim@hotmail.com>
2021-10-07 10:36:59 +0100

    Commit

---
+++ List Comprehension.py
@@ -0,0 +1,11 @@
+# List, Set, Dictionary
+
+new_list = 'helloooooya'
+my_list = [char for char in new_list]
+my_list2 = [i for i in range(0, 100)]
+my_list3 = [i * 2 for i in range(0, 100)]
+my_list4 = [i ** 2 for i in range(0, 100) if i % 2 == 0]
+print(my_list)
+print(my_list2)
+print(my_list3)
+print(my_list4)
