b458be5c02c7da39652b8d2698104fc5b974a2c1
HamzahHal <hamzah.halim@hotmail.com>
2021-10-07 20:24:09 +0100

    Commit

--- Inteview THG Prep.py
+++ Inteview THG Prep.py
@@ -9,6 +9,7 @@ x = x - y
 
 print(f'This is after the swap: {x} and {y}')
 
+
 # TCP 3 way handshake is basically a process for the client to connect to the server to create a reliable connection.to do this the client first sends a SYN (Synchronize Sequence Number)
 # to the server and
 # then the server sends a SYN + ACK to the client and then the Client sends the Acknowledgement to the server to create a reliable connection between them
@@ -16,55 +17,54 @@ print(f'This is after the swap: {x} and {y}')
 
 # Reverse Linked List
 
+def item(self, head):
+    previous, current = None, head
 
-previous, current = None, head
+    while current:
+        nxt = current.next
+        current.next = previous
+        previous = current
+        current = nxt
+    return previous
 
-while current:
-    nxt = current.next
-    current.next = previous
-    previous  = current
-    current = nxt
-return previous
 
 # Palindrome linked list
-nums = []
+def Palindrome(self, head):
+    nums = []
 
-while head:
-    nums.append(head.val)
-    head = head.next
+    while head:
+        nums.append(head.val)
+        head = head.next
 
-l, r = 0, len(nums) - 1
-while l <= r:
-    if nums[l] != nums[r]:
-        return False
-    l += r
-    r -= l
-return True
+    l, r = 0, len(nums) - 1
+    while l <= r:
+        if nums[l] != nums[r]:
+            return False
+        l += r
+        r -= l
+    return True
 
-# Palindrome in O(n) time and O(1) [constant] space
-fast = head
-slow = head
-# Find middle (slow)
-while fast and fast.next:
-    fast = fast.next.next
-    slow = slow.next
+    # Palindrome in O(n) time and O(1) [constant] space
+    fast = head
+    slow = head
+    # Find middle (slow)
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
 
-# Reverse second half
-prev = None
-while slow:
-    tmp = slow.next
-    sslow.next = prev
-    prev = slow
-    slow = tmp
+    # Reverse second half
+    prev = None
+    while slow:
+        tmp = slow.next
+        sslow.next = prev
+        prev = slow
+        slow = tmp
 
-# check palindrome
-left, right = head, prev
-while right:
-    if left.val != right.val:
-        return False
-    left = left.next
-    right = right.next
-return True
-
-
-
+    # check palindrome
+    left, right = head, prev
+    while right:
+        if left.val != right.val:
+            return False
+        left = left.next
+        right = right.next
+    return True
