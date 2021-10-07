6c1e28afe2ee73cf02a53700ba225e2fb923d78c
HamzahHal <hamzah.halim@hotmail.com>
2021-10-07 10:17:03 +0100

    Commit

--- Lambda Expression Exercise.py
+++ Lambda Expression Exercise.py
@@ -1,6 +1,11 @@
-
+# Square
 my_list = [5, 4, 3]
 
 result = list(map(lambda item: item * item, my_list))
 
 print(result)
+
+# List Sorting
+a = [(0, 2), (4, 3), (10, -1), (9, 9)]
+a.sort(key=lambda x: x[1])
+print(a)
