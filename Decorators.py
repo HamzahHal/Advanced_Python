d12c3d7f5d8ca87382664b140a3470a617fc9889
HamzahHal <hamzah.halim@hotmail.com>
2021-10-25 17:23:37 +0100

    Commit

--- Decorators.py
+++ Decorators.py
@@ -9,3 +9,40 @@ def greet():
 
 a = Hello(greet)
 print(a)
+
+
+def tester(func):
+    func()
+
+
+def tester2():
+    def func():
+        return 5
+
+    return func
+
+
+# Decorator
+def my_decorator(func):
+    def wrap_func():
+        print("******")
+        func()
+        design = input("Enter design sign: ")
+        key = len(design * 4)
+        print(design * key)
+
+    return wrap_func
+
+
+@my_decorator
+def hey():
+    print("Hey")
+
+
+@my_decorator
+def bye():
+    print("Bye")
+
+
+hey()
+bye()
